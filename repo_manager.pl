03b5171297396fb2bf9cdeee15d3c01813d24202
Nuno Martins <nuno.martins@caixamagica.pt>
2014-02-05 12:51:16 +0000

    Repo Manager is a OpenSourceProjects Crawler for Timbus that builds XML for Repo tool
    Signed-off-by: Nuno Martins <nuno.martins@caixamagica.pt>

---
+++ repo_manager.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+#
+# Copyright (c) 2014, Caixa Magica Software Lda (CMS).
+# The work has been developed in the TIMBUS Project and the above-mentioned are Members of the TIMBUS Consortium.
+# TIMBUS is supported by the European Union under the 7th Framework Programme for research and technological
+# development and demonstration activities (FP7/2007-2013) under grant agreement no. 269940.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at:   http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+# an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied, including without
+# limitation, any warranties or conditions of TITLE, NON-INFRINGEMENT, MERCHANTIBITLY, or FITNESS FOR A PARTICULAR
+# PURPOSE. In no event and under no legal theory, whether in tort (including negligence), contract, or otherwise,
+# unless required by applicable law or agreed to in writing, shall any Contributor be liable for damages, including
+# any direct, indirect, special, incidental, or consequential damages of any character arising as a result of this
+# License or out of the use or inability to use the Work.
+# See the License for the specific language governing permissions and limitation under the License.
+#
+
+use warnings;
+use strict;
+
+use Manager::Manager;
+use Manager::Crawler;
+
+
+my $manager = Manager::Manager->new();
+
+$manager->addremote("opensourceprojects", "http://opensourceprojects.eu");
+
+my $crawler = Manager::Crawler->new("http://opensourceprojects.eu/p/timbus", "http://opensourceprojects.eu");
+$crawler->init();
+
+$manager->add_repositories("git/p/timbus/", \@{$crawler->{gits}});
+
+$manager->scan_repositories("git/p/timbus/");
+$manager->save_to_xml();
